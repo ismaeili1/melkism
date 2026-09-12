1f20c27f283fadaa5b2c8c72f4c5ab1905e89658
ismaeili1 <m.ismaeili@gmail.com>
2026-09-12 05:04:02 +0330

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
-import { redirect } from "next/navigation";
+import HomeExperience from "@/components/home/HomeExperience";
 
-export default function RootPage() {
-  redirect("/fa");
+export default function HomePage(){
+
+return (
+<HomeExperience />
+);
+
 }
